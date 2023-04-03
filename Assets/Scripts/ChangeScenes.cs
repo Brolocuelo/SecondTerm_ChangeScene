fec5dfb6ce284d077e632617053a4093e5ad7bb6
Brolocuelo <luisalfonsorello@gmail.com>
2023-04-03 09:28:39 +0200

    Comienzo del Proyecto

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScenes : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+    }
+
+
+}
